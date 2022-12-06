82ab93b70b7ee680bdbb04c8d635b62f55887175
vanhien2806 <118754784+vanhien2806@users.noreply.github.com>
2022-12-06 14:29:02 +0700

    Add files via upload

---
+++ baitapTHTL_VanHien.sql
@@ -0,0 +1,52 @@
+﻿--1.	In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” sản xuất.
+SELECT MASP, TENSP
+FROM SANPHAM
+WHERE NUOCSX = 'TRUNG QUOC'
+--2.	In ra danh sách các sản phẩm (MASP, TENSP) có đơn vị tính là “cay”, ”quyen”.
+SELECT MASP, TENSP
+FROM SANPHAM
+WHERE DVT IN('CAY', 'QUYEN')
+--3.	In ra danh sách các sản phẩm (MASP,TENSP) có mã sản phẩm bắt đầu là “B” và kết thúc là “01”.
+SELECT MASP, TENSP
+FROM SANPHAM
+WHERE MASP LIKE'B%01'
+--4.	In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quốc” sản xuất có giá từ 30.000 đến 40.000.
+SELECT MASP,TENSP,NUOCSX
+FROM SANPHAM
+WHERE NUOCSX = 'TRUNG QUOC'
+AND GIA BETWEEN 30000 AND 40000
+--5.	In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” hoặc “Thai Lan” sản xuất có giá từ 30.000 đến 40.000.
+SELECT MASP, TENSP, NUOCSX
+FROM SANPHAM
+WHERE (NUOCSX = 'TRUNG QUOC' OR NUOCSX = 'THAI LAN') AND GIA BETWEEN 30000 AND 40000
+--6.	In ra các số hóa đơn, trị giá hóa đơn bán ra trong ngày 1/1/2007 và ngày 2/1/2007.
+SELECT SOHD, TRIGIA
+FROM HOADON
+WHERE NGHD >= '1/1/2007' AND NGHD <= '1/2/2007'
+--7.	In ra các số hóa đơn, trị giá hóa đơn trong tháng 1/2007, sắp xếp theo ngày (tăng dần) và trị giá của hóa đơn (giảm dần).
+SELECT SOHD, TRIGIA
+FROM HOADON
+WHERE MONTH(NGHD) = 1 AND YEAR(NGHD) = 2007
+ORDER BY NGHD ASC, TRIGIA DESC
+--8.	In ra danh sách các khách hàng (MAKH, HOTEN) đã mua hàng trong ngày 1/1/2007.
+SELECT K.MAKH, HOTEN
+FROM KHACHHANG K INNER JOIN HOADON H
+ON K.MAKH = H.MAKH
+WHERE NGHD = '1/1/2007'
+--9.	In ra số hóa đơn, trị giá các hóa đơn do nhân viên có tên “Nguyen Van B” lập trong ngày 28/10/2006.
+SELECT SOHD, TRIGIA
+FROM HOADON H INNER JOIN NHANVIEN N
+ON H.MANV = N.MANV
+WHERE NGHD = '10/28/2006'
+AND HOTEN = 'NGUYEN VAN B'
+--10.	In ra danh sách các sản phẩm (MASP,TENSP) được khách hàng có tên “Nguyen Van A” mua trong tháng 10/2006.
+SELECT DISTINCT S.MASP, TENSP
+FROM SANPHAM S INNER JOIN CTHD C
+ON S.MASP = C.MASP
+AND EXISTS(SELECT *
+FROM CTHD C INNER JOIN HOADON H
+ON C.SOHD = H.SOHD
+AND MONTH(NGHD) = 10 AND YEAR(NGHD) = 2006 AND MAKH IN(SELECT H.MAKH
+FROM HOADON H INNER JOIN KHACHHANG K
+ON H.MAKH = K.MAKH
+WHERE HOTEN = 'NGUYEN VAN A') AND S.MASP = C.MASP)
